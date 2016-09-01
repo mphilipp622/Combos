a2938e14e638e68f6062f6757c7fc11d10ac9173
Photoperiod <Wolfsenshi@gmail.com>
2016-09-01 07:12:33 -0700

    asd

---
+++ Assets/Scripts/GameMechanics/CharacterInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterInput : MonoBehaviour {
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		if(Input.GetButtonDown("Up"))
+		{
+			// start up ability functionality
+		}
+	}
+}
